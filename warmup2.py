908d537b88b8d989a7f414e98990ec6a0a18b9d4
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-27 21:30:21 -0500

    Update warmup2.py

--- warmup2.py
+++ warmup2.py
@@ -1,14 +1,14 @@
-NumberOfClasses = int(input('How many classes are you takign this semester? '))
+NumberOfClasses = int(input('How many classes are you takign this semester? ')) # get user input
 
 
-ClassesThisSemester = []
+ClassesThisSemester = [] # empty list
 
 
-for i in range(NumberOfClasses):
+for i in range(NumberOfClasses): # iterate throguh # of classes and add class
     class_name = input(f"Name of Class {i+1}: ")
     ClassesThisSemester.append(class_name)
 
 print("Classes you are taking ")
 
-for i, class_name in enumerate(ClassesThisSemester):
+for i, class_name in enumerate(ClassesThisSemester): # print classes 
     print(f"{i+1} {class_name}")
