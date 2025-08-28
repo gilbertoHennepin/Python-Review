e4192c3deb4c298b7657c81087c859ab2575bede
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-27 21:25:57 -0500

    Update students2.py

--- students2.py
+++ students2.py
@@ -2,23 +2,23 @@ from dataclasses import dataclass
 
 @dataclass
 class Student2:
-        name: str
+        name: str #define annotations
         school_id: str
         gpa: float
 
-        def __str__(self):
+        def __str__(self): #set up how info will be printed 
             return f'Name {self.name}, GPA: {self.gpa}'
 
-def main():
-    alex = Student2('Alex', 'abcdef', '2.33')
+def main(): #def main
+    alex = Student2('Alex', 'abcdef', '2.33') #define student
     print(alex.name)
     print(alex.school_id)
     print(alex)
 
-    sam = Student2('Sam', 'qwerty', '3.38')
+    sam = Student2('Sam', 'qwerty', '3.38') #define student
     print(sam)
 
-    Lam = Student2('Lam', 'lolol', '4.1')
+    Lam = Student2('Lam', 'lolol', '4.1')#define student 
     print(Lam)
     
-main()
+main() # call main method 
