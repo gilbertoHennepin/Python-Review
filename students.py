4785aae7413ce3b3dd536d87e2e4facf5d35caff
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-27 21:30:46 -0500

    Update students.py

--- students.py
+++ students.py
@@ -2,21 +2,21 @@
 
 
 class Student:
-    def __init__(self, name, school_id, gpa):
+    def __init__(self, name, school_id, gpa): #define students class
         self.name = name
         self.school_id = school_id
         self.gpa = gpa
 
-    def __str__(self):
+    def __str__(self): #set up the way info is goign to be printed
         return f'Student name: {self.name}, ID: {self.school_id}, GPA: {self.gpa}'
         
-alex = Student('Alex', 'abcdef', '2.33')
+alex = Student('Alex', 'abcdef', '2.33') #define student
 print(alex.name)
 print(alex.school_id)
 print(alex)
 
-sam = Student('Sam', 'qwerty', '3.38')
-print(sam)
+sam = Student('Sam', 'qwerty', '3.38')#define student
+print(sam) #display info 
 
-Lam = Student('Lam', 'lolol', '4.1')
-print(Lam)
+Lam = Student('Lam', 'lolol', '4.1')#define student
+print(Lam)#display info 
