5da0291067141ca9b30333e4cdb6676636270543
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-27 21:30:10 -0500

    Update Author.py

--- Author.py
+++ Author.py
@@ -1,17 +1,17 @@
-class Author:
+class Author: # define author
     def __init__(self, name,):
         self.name = name
         self.books = []
 
-    def __str__(self):
+    def __str__(self): # see if theres titles in the list and set up how info will be displayed 
         titles = ', '.join(self.books) or 'No published books'
         return f' Author name: {self.name}. Books: {titles}'
 
-    def publish(self, title):
+    def publish(self, title): #defiine and add titles to publish method
         self.books.append(title)
     
 def main():
-
+        #create author objects 
         gabriel = Author('Sam')
         gabriel.publish('Goosepumps')
         gabriel.publish('Captain Underpants')
@@ -20,7 +20,7 @@ def main():
         gil = Author('gil')
         print(gil)
 
-main()
+main() # call main method 
 
 
 
