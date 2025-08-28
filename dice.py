b8b8ca8c35670bf59bf20cc3289c63a91803678a
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-27 21:30:34 -0500

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,15 +1,15 @@
 import random 
 
 class Dice:
-    def __init__(self, sides):
+    def __init__(self, sides): # define sides
         self.sides = sides
 
-    def roll(self):
+    def roll(self):         #define rolls
         return random.randint(1, self.sides)
 
-dice = Dice(6)
+dice = Dice(6)      #declare sides of dice
 
-for roll in range(100):
+for roll in range(100):  #roll 100 times
     print(dice.roll())
 
 print(dice.roll())
